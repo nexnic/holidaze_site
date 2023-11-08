17d9701fba31e9d560eceda5b2fae86d76d165f5
Kent Erik <kent.erik.hole@outlook.com>
2023-11-08 14:03:23 +0100

    This will be the Main nav Different input only by childeren

---
+++ src/script/Layout/Navigation/Components/TempletMain.jsx
@@ -0,0 +1,17 @@
+/**
+ *  Templetmain for nav
+ * @param {*} param0 
+ * @returns 
+ */
+
+function TempletMain ({children}) {
+    return (
+        <nav className="navbar navbar-expand-lg bg-black pl-3 pr-3">
+            <div className="container-fluid">   
+                {children}
+            </div>
+        </nav>
+    )
+}
+
+export default TempletMain
