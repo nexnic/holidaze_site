ac60cfcb96def1a7d9c9ec6550dd9a47ef5182a4
Kent Erik <kent.erik.hole@outlook.com>
2023-11-08 18:42:08 +0100

    adding components function

--- src/script/Layout/Navigation/Components/TempletMain.jsx
+++ src/script/Layout/Navigation/Components/TempletMain.jsx
@@ -1,10 +1,12 @@
+
 /**
  *  Templetmain for nav
  * @param {*} param0 
  * @returns 
  */
 
-function TempletMain ({children}) {
+function TempletMain ({ children }) {
+    
     return (
         <nav className="navbar navbar-expand-lg bg-black pl-3 pr-3">
             <div className="container-fluid">   
