f215e2643fae6ac0f2af6db5ccc6d98b999cc27c
Kent Erik <kent.erik.hole@outlook.com>
2023-11-08 18:42:40 +0100

    fiksing nav

--- src/script/Layout/Layout.jsx
+++ src/script/Layout/Layout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navigation from "./Navigation/Navigation";
 
 function Layout () {
     return (
         <>
+            <Navigation />
             <Outlet/>
         </>
     )
