fdf6e5aaa133162f14bfc44446c4916dedeff796
Kent Erik <kent.erik.hole@outlook.com>
2023-11-08 13:21:09 +0100

    adding Api Request

---
+++ src/script/Components/API/RequestAPI.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+/**
+ * Request API 
+ * @returns 
+ */
+function RequesteAPI () {
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
+
+    useEffect(() => {
+        async function Requestedata(){
+            try{
+                setIsLoading(true)
+                setIsError(false)
+                const fetching = await fetch('https://api.noroff.dev/api/v1/holidaze')
+                const json = await fetching.json()
+            } catch (error){
+                console.log(error)
+                setIsError(true)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        Requestedata()
+    }, [])
+    return {data, isLoading, isLoading}
+}
+
+export default RequesteAPI
