152db8b67bcc9b2c7e699bd406627e3243855ee0
Kent Erik <kent.erik.hole@outlook.com>
2023-11-15 15:59:59 +0100

    auth api call

---
+++ src/script/Components/API/Auth/RequestAPIAuth.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+function RequestAPIAuth (UserName,Token){
+    console.log(UserName,Token)
+
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
+
+    useEffect(() => {
+        async function getData(){
+
+            try {
+                setIsLoading(true);
+                setIsError(false)
+
+                const fetchedData = await fetch(`https://api.noroff.dev/api/v1/holidaze/profiles/${UserName}`,{
+                    method: 'GET',
+                    headers:{
+                        Authorization: `Bearer ${Token}`,
+                        'Content-type': 'application/json; charset=UFT-8',
+                    }
+                })
+                const responseData = await fetchedData.json()
+                console.log(responseData)
+
+            } catch (error) {
+                console.log(error)
+            }
+            finally {
+                setIsLoading(false)
+            }
+        }
+        getData()
+    }, [UserName, Token])
+    return {data, isLoading, isError}
+}
+
+export default RequestAPIAuth
