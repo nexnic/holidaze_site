90616a85832a4179d5737eeeaa2d59ea5f7856c9
Kent Erik <kent.erik.hole@outlook.com>
2023-11-15 15:59:34 +0100

    adding profile

--- src/script/Pages/Profile/Profile.jsx
+++ src/script/Pages/Profile/Profile.jsx
@@ -1,5 +1,25 @@
+import { useParams} from 'react-router-dom';
+import GetLocal from '../../Storage/GetLocal';
+import RequestAPIAuth from '../../Components/API/Auth/RequestAPIAuth';
+import ProfileImage from './Components/ProfileImage';
+import ProfileUserInfo from './Components/ProfileUserInfo';
+
+
 function Profile (){
-    console.log('profile')
+    const {userID} = useParams()
+    const UserData = GetLocal('userData')
+    const {accessToken} = UserData
+    const {data, isLoading, isError} = RequestAPIAuth(userID, accessToken)
+    if(isLoading) return<div>loading</div>
+    if(isError) return <div>Error</div>
+    else {
+        return (
+            <main>
+                <ProfileImage avatar={data.avatar} userName={data.name}/>
+                <ProfileUserInfo userName={data.name} userEmail={data.email}/>
+            </main>
+        )
+    }
 }
 
-export Profile
+export default Profile
