9d6d18b1f43e65f0d91fe23e5c0077f3338ad66f
Kent Erik <kent.erik.hole@outlook.com>
2023-11-18 14:07:45 +0100

    starting on media

--- src/script/Pages/Venue/Components/VenueCarousel.jsx
+++ src/script/Pages/Venue/Components/VenueCarousel.jsx
@@ -1,5 +1,11 @@
 function VenueCarousel({media}) {
-    
+    return (
+        <div className="carousel slide">
+            <div className="carousel-inner">
+                
+            </div>
+        </div>
+    )
 }
 
 export default VenueCarousel
