70204afb82b65751abeb9a90cfeadc7fb3b7dec2
Kent Erik <kent.erik.hole@outlook.com>
2023-11-23 14:41:50 +0100

    fix the profile Menu

--- src/script/Layout/Navigation/Components/TempletProfileMenu.jsx
+++ src/script/Layout/Navigation/Components/TempletProfileMenu.jsx
@@ -18,7 +18,7 @@ function TempletProfileMenu () {
             <div className='rounded-circle' role="button" id='ProfileDropdown' data-bs-toggle='dropdown' aria-expanded='false'>
                 <img src="" alt="" className='rounded-circle' style={{width: '20px', height:'20px'}}/>
             </div>
-            <ul className='dropdown-menu' aria-labelledby='ProfileDropdown'>
+            <ul className='dropdown-menu dropdown-menu-dark' aria-labelledby='ProfileDropdown'>
                 <li>
                     <Button TypeOf={'button'} ClassOf={'dropdown-item'} OnClick={HandlerProfileButton}>Profile</Button>
                 </li>
