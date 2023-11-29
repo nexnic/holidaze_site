fd8bcb3da299c60df812a547b73f0887eea1d629
Kent Erik <kent.erik.hole@outlook.com>
2023-11-29 20:18:47 +0100

    fix

--- src/script/Pages/Profile/Components/ProfileImage.jsx
+++ src/script/Pages/Profile/Components/ProfileImage.jsx
@@ -1,18 +1,20 @@
+import { useState } from "react"
+import ChangeAvatarForm from "../../../Components/Form/ChangeAvatarForm"
+
 function ProfileImage ({avatar, userName}) {
-    console.log(avatar, userName)
+    const [showInput, setShowInput] = useState(false)
+
+    const handleMenu = () => setShowInput(!showInput)
 
     return (
-
         <div>
             <div className="d-flex justify-content-center mb-4">
                 <img id="selectedAvatar" src="https://source.unsplash.com/random/150x150?person"
                 className="rounded-circle" style={{width: '200px', height: '200px', }} alt="example placeholder"/>
             </div>
+            {showInput ? <ChangeAvatarForm /> : null}
             <div className="d-flex justify-content-center">
-                <div className="btn btn-primary btn-rounded">
-                    <label className="form-label text-white m-1" htmlFor="customFile2">Choose file</label>
-                    <input type="file" className="form-control d-none" id="customFile2" />
-                </div>
+                <button className="btn btn-primary" onClick={handleMenu}>{showInput ? 'close' : 'Change image'}</button>
             </div>
         </div>
         
