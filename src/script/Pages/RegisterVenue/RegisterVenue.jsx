e9e8f9198f42ac5824aec5e51ef0bf143edc36ea
Kent Erik <kent.erik.hole@outlook.com>
2023-12-04 12:55:24 +0100

    fix2

--- src/script/Pages/RegisterVenue/RegisterVenue.jsx
+++ src/script/Pages/RegisterVenue/RegisterVenue.jsx
@@ -1,7 +1,11 @@
-import RegisterVenueForm from "../../Components/Form/RegisterVenueForm"
+import { useState } from "react";
+import ModelBox from "../../Components/Reuse/ModelBox"
+import RegisterVenueManger from "../../Components/Form/RegisterVenueManager";
+
 
 function RegisterVenue () {
-
+    const [modalShow, setModalShow] = useState(false);
+    const handleClose = () => setModalShow(false);
 
     return (
         <main className='container'>
@@ -11,9 +15,27 @@ function RegisterVenue () {
                         <h2 style={{color: 'white'}}>Welcome to Holidaze</h2>
                         <p style={{color: 'white'}}>Are you a venue owner looking to showcase your space for events? Register your venue with us and join our vibrant community of event spaces. We make it easy for event organizers to discover and book venues that suit their needs. Follow the simple steps below to get started:</p>
                     </div>
-                    <RegisterVenueForm/>
+                    <div style={{color: 'white'}}>
+                        <h3 style={{color: 'white'}}>Benefits of Registering with Us:</h3>
+                        <p>Increased Visibility: Showcase your venue to a broader audience.</p>
+                        <p>Streamlined Booking: Receive booking requests directly through our platform</p>
+                        <p>Networking Opportunities: Connect with event organizers seeking unique spaces</p>
+                        <p>Ready to get started?</p>
+                        <button className="btn btn-primary" onClick={() => setModalShow(true)}>
+                            Become Venue Owner
+                        </button>
+                    </div>
                 </div>
             </div>
+                
+            <ModelBox
+                IsActive={modalShow}
+                Title='Become Venue'
+                Onhide={handleClose}
+                >
+                    <RegisterVenueManger/>
+            </ModelBox>
+                
         </main>
     )
 
