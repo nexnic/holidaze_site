fbabb2220cad83baf7544a9140206054cadea7f8
Kent Erik <kent.erik.hole@outlook.com>
2023-12-05 13:05:41 +0100

    fix

--- src/script/Pages/Home/Home.jsx
+++ src/script/Pages/Home/Home.jsx
@@ -1,8 +1,21 @@
+import ChangeHead from "../../Components/Head/ChangeHead"
+
+
 
 function Home () {
+
+    const meta = {
+        title:'Holidaze | Home',
+        description: 'Discover the perfect holiday venue for your festive celebrations at Holidaze Venues',
+        keywords: 'Holidaze, holiday, parties, gatherings, events', 
+    }
+    ChangeHead(meta)
+
+
+
     return (
         <>
-            <h1>test</h1>
+            <h1 style={{color: 'white'}}>test</h1>
         </>
     )
 }
